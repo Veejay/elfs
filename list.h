6e4194b5e1e4e0c811609be69f7c82c31510ef83
Guillaume Leconte <guillaume@bk.ru>
2012-08-15 18:10:15 +0200

    cleanup: use the typedef alias in list.h prototypes

--- list.h
+++ list.h
@@ -11,14 +11,14 @@ struct list *list_new(void);
 typedef void (* tlist_free_func)(void *);
 typedef int (* tlist_cmp_func)(void *, void *);
 
-void list_set_free_func(struct list *list, tlist_free_func free_func);
-void list_set_cmp_func(struct list *list, tlist_cmp_func cmp_func);
+void list_set_free_func(tlist *list, tlist_free_func free_func);
+void list_set_cmp_func(tlist *list, tlist_cmp_func cmp_func);
 
-int list_get_size(struct list *q);
-void list_add(struct list *q, void *elem);
-void list_add_uniq(struct list *q, void *elem);
-void list_extract(struct list *q, void *elem);
-void *list_get(struct list *q, void *elem);
+int list_get_size(tlist *q);
+void list_add(tlist *q, void *elem);
+void list_add_uniq(tlist *q, void *elem);
+void list_extract(tlist *q, void *elem);
+void *list_get(tlist *q, void *elem);
 void *list_get_nth(tlist *q, int n);
 
 #endif /* LIST_H */
